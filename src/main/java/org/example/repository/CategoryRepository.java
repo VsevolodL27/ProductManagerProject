4f043b00355ca4b780e48694b959e72a6ecc14c8
VsevolodL27 <lavrenov.vv@edu.spbstu.ru>
2023-08-30 15:07:56 +0300

    Created Category.java, CategoryRepository.java and CategoryService.java.

---
+++ src/main/java/org/example/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+import org.example.entity.Category;
+import org.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Product> findCategoryId(Long id);
+}
