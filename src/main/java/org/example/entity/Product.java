28646839d72f51566e57f8a7df64930bd8224550
VsevolodL27 <lavrenov.vv@edu.spbstu.ru>
2023-08-30 14:16:46 +0300

    Added entity package. Created Product.java.

---
+++ src/main/java/org/example/entity/Product.java
@@ -0,0 +1,25 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Short amount;
+}
